da06b5a8f5fa74eb07ba31d9aa0b07d3403fcd76
sparrownut <31125137+sparrownut@users.noreply.github.com>
2022-07-14 15:08:00 +0800

    SSH crack

---
+++ msfconfig.py
@@ -0,0 +1,8 @@
+import struct.level
+
+msfPassword = "test"
+msfUsername = "msf"
+msfHost = "127.0.0.1"
+msfPort = 55553
+msfSSLChoice = False
+level = struct.levels.LOW
--- scan/smbscan.py
+++ scan/smbscan.py
@@ -1,25 +1,25 @@
 import socket
 
 
-class sshScan:
-    def conn_port(self,host, port):
+class smbScan:
+    def conn_port(self, host, port):
         sk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sk.settimeout(2)  # 设置超时时间
         try:
             sk.connect((host, port))
-            recv = sk.recv(1024)
             sk.close()
-            if b"SSH" in recv:
-                return True
-            else:
-                return False
+            return True
         except Exception:
             sk.close()
             return False
 
-    def ssh_scan(self,host, port):
+    def smb_scan(self, host, port):
         res = self.conn_port(host, port)
         if res:
-            print(" [+] " + host + "在" + str(port) + "端口上的SSH服务开启")
+            print(" [+] " + host + "在" + str(port) + "端口上的SMB端口开启")
             return True
         return False
+
+
+smbscan = smbScan()
+smbscan.smb_scan("10.133.73.212", 445)
